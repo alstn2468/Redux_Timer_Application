2e89389c9f55b116122106399c015c9d72af2a55
김민수 <alstn2468_@naver.com>
2019-11-13 20:46:43 +0900

    #17 Fix button style using icon -> using text

--- tomato-timer/components/Button/index.js
+++ tomato-timer/components/Button/index.js
@@ -1,19 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { TouchableOpacity } from "react-native";
-import { FontAwesome } from "expo-vector-icons";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 
-const Button = ({ iconName, onPress }) => {
+const Button = ({ buttonName, onPress }) => {
     return (
-        <TouchableOpacity onPressOut={onPress}>
-            <FontAwesome name={iconName} size={80} color="#FFFFFF" />
+        <TouchableOpacity onPressOut={onPress} style={styles.container}>
+            <View style={styles.buttonView}>
+                <Text style={styles.buttonText}>{buttonName}</Text>
+            </View>
         </TouchableOpacity>
     );
 };
 
 Button.propTypes = {
-    iconName: PropTypes.string.isRequired,
+    buttonName: PropTypes.string.isRequired,
     onPress: PropTypes.func.isRequired
 };
+
+const styles = StyleSheet.create({
+    container: {
+        width: "80%",
+        height: "50%",
+        justifyContent: "center"
+    },
+    buttonView: {
+        width: "100%",
+        height: 100,
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20
+    },
+    buttonText: {
+        fontSize: 40,
+        color: "#CE0B24"
+    }
+});
 
 export default Button;
